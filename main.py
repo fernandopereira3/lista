25a4fa4974007ba100164b5c4208987da3a8eb37
Fernando Pereira <fernandopereira-3@hotmail.com>
2025-02-17 17:11:01 -0300

    sei la

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
 from flask import Flask, render_template, request
 import pandas as pd
+import pymongo
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/lista', methods=['GET', 'POST'])
 def pesquisa_visi():
     if request.method == 'POST':
-        # Read Excel file
-        df = pd.read_excel('visitas.xlsx')
-        
-        # Get search term from form
-        search_term = request.form.get('search')
-        
-        # Filter dataframe
-        result = df[df['Nome'].str.contains(search_term, case=False, na=False)]
-        
-        return render_template('results.html', results=result.to_dict('records'))
-    
-    return render_template('search.html')
+       pass
 
 if __name__ == '__main__':
     app.run(debug=True)
