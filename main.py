d865a82402ddf8b7c4c5defbfd34a1a531884936
Fernando <fernandopereira-3@hotmail.com>
2025-02-17 22:50:51 -0300

    nao faco ideia doq to fazendo

--- main.py
+++ main.py
@@ -13,46 +13,20 @@ try:
     db = client["cpppac"]
     collection = db["sentenciados"]
 except pymongo.errors.ConnectionFailure as e:
-    print(f"Could not connect to MongoDB: {e}")
-    # Handle the error appropriately, e.g., display an error page
-    exit(1)  # Or some other error handling
+    print(f"Nao foi possivel conectar ao banco: {e}")
+    exit(1)
 
 
 @app.route('/lista', methods=['GET', 'POST'])
-def normalizar_matricula(matricula):
-    return re.sub(r'[\.\-]', '', matricula).strip() 
 
-def pesquisa_visi():
-    if request.method == 'POST':
-        matricula_normalizada = re.compile(f".*{txt_d_pesquisa}.*", re.IGNORECASE)
-        consulta_exata = {'matricula': matricula_normalizada}
+#def normalizar_matricula():
+#    matricula = request.form.get('pesquisa_preso')
+#    debug = app.logger.info(matricula)
+#    return debug
 
-        # { "<field>": { "$regex": "pattern", "$options": "<options>" } } REGEX DO MONGODB
-        
-        if not resultado:
-            resultado = list(sentenciados.find({'matricula': {'$regex': matricula_normalizada}}))  
-            return resultado
 
-        query = {
-            'detento': {
-                'matricula': "matr",
-                '$lte': "data_fim"
-            }
-        }
-
-        try:
-            resultado = list(sentenciados.find(query))
-
-            # Convert ObjectId to string using list comprehension
-            results = [{**result, '_id': str(result['_id'])} for result in results]
-
-            return render_template('pesquisa.html', results=results)  # Pass results to template
-
-        except pymongo.errors.PyMongoError as e:
-            print(f"Database error: {e}")
-            return render_template('pesquisa.html', error="A database error occurred.")
-
-    return render_template('pesquisa.html')  # Render the page initially or if not a POST request
+def pesquisa_matricula():
+  return render_template('pesquisa.html')  # Render the page initially or if not a POST request
 
 
 if __name__ == '__main__':
