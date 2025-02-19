1033d68287d4311bb11f01ed11f409f32af44e3f
Fernando <fernandopereira-3@hotmail.com>
2025-02-19 16:31:36 -0300

    estamos andando!

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, jsonify
 from models.forms import PesquisaForm
 import pandas as pd
 import  os
+import re
+import datetime
 from pymongo import MongoClient
 from flask_pymongo import PyMongo
 from flask_wtf import FlaskForm
@@ -22,30 +24,49 @@ class PesquisaForm(FlaskForm):
     nome = StringField('Nome')
     pesquisar = SubmitField('PESQUISAR')
 
-@app.route('/lista', methods=['GET', 'POST'])
+
+@app.route('/lista', methods=['GET',  'POST'])
 def pesquisa_matricula():
     form = PesquisaForm()
-    sentenciados = db.sentenciados  # Acesse a coleção do MongoDB
-    resultados = []  # Inicializa uma lista vazia para os resultados
+    sentenciados = db.sentenciados
+    resultados = []
 
-    if form.validate_on_submit():  # Se o formulário foi enviado corretamente
+    if form.validate_on_submit():
         matricula = form.matricula.data.strip()
+        #matri_norm = re.sub(r'[\.\-]', '', matricula).strip()
         nome = form.nome.data.strip()
         query = {}
-
+#re.compile(f".*{txt_d_pesquisa}.*", re.IGNORECASE)
         if matricula:
-            query['matricula'] = matricula
+            query['matricula'] = re.compile(f".*{matricula}.*", re.IGNORECASE)
         if nome:
-            query['nome'] = {"$regex": nome, "$options": "i"}  # Busca por nome ignorando maiúsculas e minúsculas
+            query['nome'] = {"$regex": nome,  "$options": "i"}
 
-        resultados = list(sentenciados.find(query))  # Converte o cursor do MongoDB para lista
+        resultados = list(sentenciados.find(query))
 
-        # Convertendo ObjectId para string antes de passar para o template
         for resultado in resultados:
             resultado['_id'] = str(resultado['_id'])
 
     return render_template('pesquisa.html', form=form, sentenciados=resultados)
 
+
+@app.route('/adicionar/<matricula>', methods=['POST'])
+def adicionar_lista(matricula):
+    sentenciado = db.sentenciados.find_one({'matricula': matricula})
+    if sentenciado:
+        lista_selecionados = db.lista_selecionados
+        lista_selecionados.insert_one({
+            'nome': sentenciado['nome'],
+            'matricula': sentenciado['matricula'],
+            'data_adicao': datetime.datetime.now()
+        })
+        return jsonify({'status': 'success', 'message': 'Adicionado com sucesso'})
+    return jsonify({'status': 'error', 'message': 'Matrícula não encontrada'})
+
+@app.route('/lista-selecionados', methods=['GET'])
+def visualizar_lista():
+    lista_selecionados = db.lista_selecionados.find()
+    return render_template('lista.html', lista=lista_selecionados)
 
 
 @app.route('/completa', methods=['GET'])
